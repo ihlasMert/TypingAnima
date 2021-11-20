b7e8c9855cec15f64e7b7413f8466fb5a2c1fc66
İhlas Mert <ihlas1mert@gmail.com>
2021-11-20 23:51:44 +0300

    Initial Commit

---
+++ TypingAnimations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TypingAnimations
+//
+//  Created by ihlas on 20.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
